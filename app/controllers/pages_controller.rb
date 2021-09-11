6d29b725f543e2d3e9d610f4225ecdcce3c4f72b
Caio Ramos <kaineo@hotmail.com>
2021-09-10 22:43:07 -0300

    [+] Homepage layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!
+  before_action :is_user_logged_in?
 
-  def home
+  def home; end
+
+  private
+
+  def is_user_logged_in?
+    redirect_to portfolios_path if current_user
   end
 end
